cb7e5fec02f83b855e0f9998fc2858af18e58b8b
Moon Juhan <codemike@naver.com>
2023-03-27 14:38:52 +0900

    feat: Add SceneImages component

---
+++ src/components/views/view-basic/SceneImages.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="scene-images">
+    <h2>Scene Images</h2>
+
+    <div class="image-grid">
+      <div class="image-wrapper" v-for="sceneImage in sceneImages" :key="sceneImage.uuid">
+        <img :src="sceneImage.url" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  thdModelJSON: {
+    type: Object,
+    required: true,
+  },
+})
+
+const sceneImages = ref([])
+
+const refineImages = (json = {}) => {
+  const { images } = json.scene || {}
+
+  sceneImages.value = images
+}
+
+watch(
+  () => props.thdModelJSON,
+  (json) => {
+    refineImages(json)
+  }
+)
+</script>
+
+<style lang="scss" scoped>
+.scene-images {
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+}
+</style>
